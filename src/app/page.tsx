944786e49dd3efa6e535bc7e1aa514107335ab47
mohammad-hadi <linub7@gmail.com>
2025-01-20 13:34:14 +0330

    add shadcn to project / add theme toggle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import ThemeToggle from '@/components/shared/theme-toggle';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold text-blue-500">Welcome to the course</h1>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="text-blue-500 font-barlow">Welcome to the course</h1>
+      <Button>Click here</Button>
     </div>
   );
 }
